da27545bac5a67eabfb4febf387995a33a1690c9
Xenturion-ctrl <62330246+Xenturion-ctrl@users.noreply.github.com>
2020-04-02 15:22:50 +0200

    EXO4
    EXO4

---
+++ EXO4.php
@@ -0,0 +1,31 @@
+<?php
+$title='Ex4';
+include "include/header.php";
+?>
+<a href="?l=2&c=2">Tableau 2 x 2</a><br>
+<a href="?l=5&c=5">Tableau 5 x 5</a><br>
+<a href="?l=10&c=8">Tableau 10 x 8</a><br>
+<hr>
+<?php
+//Récupère les paramètres l et c passés dans l'url
+$nblignes=$_GET["l"]??10;
+$nbcolonnes=$_GET["c"]??10;
+echo "<h2>Tableau de $nblignes par $nbcolonnes</h2>";
+?>
+<table border='1'>
+    <tbody>
+      <?php for($i=0;$i<$nblignes;$i++){
+        $gras=($i%2==0)?"bold":"normal";
+      ?>
+        <tr>
+        <?php for($j=0;$j<$nbcolonnes;$j++){
+            $rouge=($j%2==0)?"red":"black";
+            ?>
+            <td style="font-weight: <?=$gras ?>;color:<?=$rouge ?>"><?="$i - $j"?></td>
+        <?php }?>
+        </tr>
+      <?php }?>
+    </tbody>
+</table>
+<?php
+incl
